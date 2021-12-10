5991890942b8183fb62b2dbc7ec6550424099185
it-uni <dh@it-universe.ru>
2021-12-10 08:52:40 +0300

    DMZ

---
+++ dmz.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+#отключаем SELinux
+sed -i 's/^SELINUX=.*/SELINUX=disabled/g' /etc/selinux/config
+setenforce 0
+
+#отключаем firewall.d
+systemctl stop firewalld
+systemctl disable firewalld
+
+#ставим нужные пакеты
+yum install -y net-tools zip unzip mc wget yum-utils telnet epel-release git git-lfs curl
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sed -i 's/$releasever/7/g' /etc/yum.repos.d/docker-ce.repo
+yum -y update
+yum install -y docker-ce docker-ce-cli containerd.io
+systemctl enable docker
+systemctl start docker
+
+#добавляем docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+#создаем каталоги контейнеров
+cd /opt
+mkdir FE20
+cd FE20
+mkdir spring && mkdir ./spring/logs && mkdir ./spring/etc && mkdir ./spring/file-exchanger && mkdir ./spring/temp
+mkdir react && mkdir ./react/front/ && mkdir ./react/logs
+mkdir postgres && mkdir ./postgres/pgdata && mkdir ./postgres/scripts
+
+#скачиваем и распаковываем стартовые скрипты
+cd /opt/FE20
+wget https://github.com/it-uni/files/raw/main/dmz.zip
+unzip dmz.zip
+
+#копируем файлы по папкам
+cp -R front/* /opt/FE20/react/front/
+cp ROOT.war /opt/FE20/spring/
+cp *.sql /opt/FE20/postgres/scripts/
+cp *.yml /opt/FE20/
+cp Dockerfile /opt/FE20/spring/
+cp nginx.conf /opt/FE20/react/
+cp temp/* /opt/FE20/spring/temp/
+
+#разворачиваем контейнеры
+cd /opt/FE20 && docker-compose up --build -d
